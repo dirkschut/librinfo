d214b0beae8d96286df780c06e3b54275b6fe9f5
Dirk Schut <dschut@outlook.com>
2018-02-01 13:59:32 +0100

    Very basic person logging

--- LibrInfo.sol
+++ LibrInfo.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.4.0;
 
-contract LibrInfo{
-  
+contract LibrInfo {
+  struct Persoon {
+    bytes32 id;
+    bool magBoekLenen;
+    bool magBoekToevoegen;
+  }
+
+  address public eigenaar;
+  mapping (bytes32 => Persoon) public personen;
+  bytes32[] public personenlijst;
+
+  function LibrInfo() public {
+    eigenaar = msg.sender;
+  }
+
+  function PersoonToevoegen(bytes32 nummer, bytes32 code) public{
+    require (msg.sender == eigenaar);
+    personen[keccak256(nummer, code)] = Persoon({id: keccak256(nummer, code), magBoekLenen: true, magBoekToevoegen: false});
+    personenlijst.push(keccak256(nummer, code));
+  }
+
+  function GetPersoon(bytes32 nummer, bytes32 code) view public returns (bool){
+    return (personen[keccak256(nummer, code)].magBoekLenen);
+  }
 }
