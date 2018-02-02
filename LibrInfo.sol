6dc7ec45eba42003e14356029388f4cad3b62e01
Dirk Schut <dschut@outlook.com>
2018-02-02 10:13:09 +0100

    Basic book system

--- LibrInfo.sol
+++ LibrInfo.sol
@@ -7,9 +7,18 @@ contract LibrInfo {
     bool magBoekToevoegen;
   }
 
+  struct Boek {
+    string ISBN;
+    string titel;
+    string auteur;
+  }
+
   address public eigenaar;
+
   mapping (bytes32 => Persoon) public personen;
   bytes32[] public personenlijst;
+
+  Boek[] public boekenlijst;
 
   function LibrInfo() public {
     eigenaar = msg.sender;
@@ -21,7 +30,17 @@ contract LibrInfo {
     personenlijst.push(keccak256(nummer, code));
   }
 
+  function PersoonMagToevoegen(bytes32 nummer, bytes32 code) public{
+    require (msg.sender == eigenaar && personen[keccak256(nummer, code)].magBoekLenen);
+    personen[keccak256(nummer, code)].magBoekToevoegen = true;
+  }
+
   function GetPersoon(bytes32 nummer, bytes32 code) view public returns (bool){
     return (personen[keccak256(nummer, code)].magBoekLenen);
+  }
+
+  function BoekToevoegen(bytes32 nummer, bytes32 code, string ISBN, string titel, string auteur) public{
+    require (personen[keccak256(nummer, code)].magBoekToevoegen);
+    boekenlijst.push(Boek({ISBN: ISBN, titel: titel, auteur: auteur}));
   }
 }
